f56888a92fcd74289d63d3654fda157fd0a2bc2b
estebandalelr <estebandalelr@gmail.com>
2017-09-22 12:54:59 -0500

    Working commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongodb =require('mongodb');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var url ="mongodb://165.227.187.208:4536/tenants";
+
+function getTenants(callback){
+  mongodb.connect(url, (err,db) => {
+    if (err) throw err;
+
+    var tenants =  db.collection("tenants");
+    tenants.find({}).toArray((err2, tenants)=>{
+      if (err2) throw err2;
+
+      callback(tenants);
+
+    });
+  });
+}
+
+/* GET tenants */
+router.get('/tenants', function(req, res) {
+  getTenants((tenants) => {
+    res.json(tenants);
+  });
 });
 
 module.exports = router;
